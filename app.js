a748df6e5191993c4257168002e5824afb771d81
MuhammadAsghar1999 <143824430+MuhammadAsghar1999@users.noreply.github.com>
2023-09-19 00:45:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+// var arr =["Nokia","apple","sumsung","oppo","redmi"]
+// document.write("<select>" + "<option>" + arr[0] + "</option>" + "<option>" + arr[1] + "</option>" + "<option>" + arr[2] + "</option>" + "<option>" + arr[3] + "</option>" + "<option>" + arr[4] + "</option>" +"</selected>")
+
+// var arr =["faraz","hamza","asghar","hassan",100,true]
+// console.log(arr[1])
+
+// var userInput = prompt("Enter value to add the last of arrey")
+// var userInput1 = prompt("Enter value to add the last of arrey")
+// var userInput2 = prompt("Enter value to add the last of arrey")
+// var userInput3 = prompt("Enter value to add the last of arrey")
+// var userInput4 = prompt("Enter value to add the last of arrey")
+
+// var animals =["dog","cheetah","lion","snake","crocodile","elephant"]
+// // animals.push(userInput,userInput1,userInput2,userInput3,userInput4)
+
+// console.log("ORIGNAL ARRY",animals)
+// var startingvalue = +prompt("Enter the value =to start")
+// var endingvalue = +prompt("Enter ending value") 
+// var addingElement = +prompt("Enter Element")
+// var addingElement1 = +prompt("Enter Element")
+// var addingElement2 = +prompt("Enter Element")
+// animals.slice(startingvalue,endingvalue,addingElemen,addingElemen1,addingElemen2)
+
+// console.log("ORIGNAL ARRY",animals)
+// var targetIndex = +prompt("Enter the value =to start")
+// var deletedIndex = +prompt("Enter ending value")
+// var addingElements = +prompt("Enter Element")
+// var addingElements2 = +prompt("Enter Element")
+// var addingElements3 = +prompt("Enter Element")
+
+
+//   loop chapter
+
+// for (var i =1; i <=10; i++){
+//     console.log("welcome user",i);
+// }
+
+
+// for (var i =0; i <=10; i=i+2){
+//     console.log("welcome user",i);
+// }
+
+
+// var userInput = +prompt("Enter a number to generate a multiplication table")
+// var tableLength = +prompt("Enter table length")
+// for(var i =1; i<= tableLength; i++){
+//     document.write(userInput + "x" + i +"="+ userInput * i + "<br>")
+// }
+
+// var arr = ["abc",100,true,false,"xyz"]
+// for(var i = 0; i < arr.length; i++ ){
+//     console.log(arr[i])
+// }
+//  var fruits =["banana","apple","grapes","mango",]
+//  for(var i=0;i< fruits.length;i++){
+//  console.log("Element of " + fruits [i] + "at" + "" + i)
+// }
+
+// Q:6
+
+// var countingSeries = "";
+// for (var i = 1; i <= 15; i++) {
+//     countingSeries += i + " ,";
+// }
+// document.write("countingSeries").textContent = countingSeries;
+
+// Reverse Counting Series
+// var reverseCountingSeries = "2";
+// for (var i = 10; i >= 1; i--) {
+//     reverseCountingSeries += i + ", ";
+// }
+// document.getElementById("reverseCountingSeries").textContent = reverseCountingSeries;
+
+// // Even Series
+// var evenSeries = "";
+// for (var i = 0; i <= 20; i += 2) {
+//     evenSeries += i + ", ";
+// }
+// document.getElementById("evenSeries").textContent = evenSeries;
+
+// // Odd Series
+// var oddSeries = "";
+// for (var i = 1; i <= 19; i += 2) {
+//     oddSeries += i + ", ";
+// }
+// document.getElementById("oddSeries").textContent = oddSeries;
+
+// // Custom Series
+// var customSeries = "";
+// for (var i = 2; i <= 20; i += 2) {
+//     customSeries += i + "k, ";
+// }
+// document.getElementById("customSeries").textContent = customSeries;
+
+
+// Q:7
+// const A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// const userInput = prompt("Enter an item to search for:");
+// if (A.includes(userInput)) {
+//     alert(`Yes, '${userInput}' is found in the list.`);
+// } else {
+//     alert(`No, '${userInput}' is not found in the list.`);
+// }
+
+// Q:8
+
+// const a = [24, 53, 78, 91, 12];
+// let largestNumber = a[0];
+// for (let i = 1; i < a.length; i++) {
+//     if (a[i] > largestNumber) {
+//         largestNumber = a[i];
+//     }
+// }
+// console.log(`The largest number in the array is: ${largestNumber}`);
+// In 
+
+// Q:9
+
+// var A = [24, 53, 78, 91, 12];
+// var smallestNumber = A[0];
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] < smallestNumber) {
+//         smallestNumber = A[i];
+//     }
+// }
+// console.log(`The smallest number in the array is: ${smallestNumber}`);
